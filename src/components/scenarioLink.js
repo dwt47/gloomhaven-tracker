5fd1ce2b84bbae05e98f1efa7e6710c19c13bcb4
Daniel Thompson <DWT101092@gmail.com>
2020-04-06 20:58:22 -0400

    test

--- src/components/scenarioLink.js
+++ src/components/scenarioLink.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
+import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
 
 const ScenarioLink = ({ scenario: { path, title, scenarioID } }) => (
-	<Link to={path}>{scenarioID} {title}</Link>
-);
+  <Link to={path}>
+    {scenarioID} {title}
+  </Link>
+)
 
 ScenarioLink.propTypes = {
-	scenario: PropTypes.shape({
-		path: PropTypes.string.isRequired,
-		title: PropTypes.string.isRequired,
-		scenarioID: PropTypes.number,
-	}).isRequired,
-};
+  scenario: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    scenarioID: PropTypes.number,
+  }).isRequired,
+}
 
-export default ScenarioLink;
+export default ScenarioLink
